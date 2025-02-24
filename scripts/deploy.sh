f00a0f4a88a86dbd044f3e33fa5ebfeb634358b5
root <root@ip-10-0-1-242.ap-northeast-2.compute.internal>
2025-02-24 06:54:13 +0000

    please

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,13 @@
 
 # Git 리포지토리 클론
 cd /home/ec2-user/app
-git clone https://github.com/your-username/your-repository.git
+git clone https://github.com/realyun99/realyun-asg-deploy.git
 
 # 또는 SSH 키를 이용한 클론
 # git clone git@github.com:your-username/your-repository.git
 
 # 필요한 파일 복사, 서버 설정 등
-cp /home/ec2-user/app/your-repository/index.html /var/www/html/index.html
+cp /home/ec2-user/app/realyun-asg-deploy/index.html /var/www/html/index.html
 
 # Apache 서버 재시작 (필요시)
 sudo systemctl restart httpd
